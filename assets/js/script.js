538c7be03ddeb27c3d1810ea6c5c6dce809950dd
Hunter Bell <bellhunter512@gmail.com>
2022-04-18 18:16:51 -0400

    user can create new tasks with types

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,18 @@ var tasksToDoE1 = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
     event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
     var taskItemE1 = document.createElement("li");
     taskItemE1.className = "task-item";
-    taskItemE1.textContent = "This is a new task.";
+
+    var taskInfoE1 = document.createElement("div");
+    taskInfoE1.className = "task-info";
+
+    taskInfoE1.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+    taskItemE1.appendChild(taskInfoE1);
     tasksToDoE1.appendChild(taskItemE1);
 }
 
